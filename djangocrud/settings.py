651a0e75503bf85012746b50390616a8cadf2e5b
Cristhian J Garcia Leiva <cristhianjgl16@gmail.com>
2022-10-27 01:17:26 -0500

    fix: add static url

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#dev
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/' #este queda en prod y dev
+
 #production
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
